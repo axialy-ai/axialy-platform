b440ffea2633b3df671592e7ac31e8ad08fd3811
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-18 16:36:10 -0700

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,12 @@
+output "droplet_ips" {
+  value = {
+    ui    = digitalocean_droplet.ui.ipv4_address
+    api   = digitalocean_droplet.api.ipv4_address
+    admin = digitalocean_droplet.admin.ipv4_address
+  }
+}
+
+output "mysql_connection" {
+  value = digitalocean_database_cluster.mysql.host
+}
 
