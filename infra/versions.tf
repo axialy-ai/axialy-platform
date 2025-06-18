403409725eb1305b1e34911a66de8308d8379152
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-18 16:33:30 -0700

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,1 +1,10 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.56.0"   # Latest as of June 2025
+    }
+  }
+}
 
