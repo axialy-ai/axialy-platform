ac6bfde0af829febd9db5190aed7d80e4b120605
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:55:09 -0700

    Create outputs.tf

---
+++ infra/outputs.tf
@@ -0,0 +1,8 @@
+output "droplet_ips" {
+  value = {
+    admin = digitalocean_droplet.admin.ipv4_address
+    ui    = digitalocean_droplet.ui.ipv4_address
+    api   = digitalocean_droplet.api.ipv4_address
+    root  = digitalocean_droplet.root.ipv4_address
+  }
+}
