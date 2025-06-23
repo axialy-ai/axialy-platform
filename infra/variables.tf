470f00389e7d7ac048ba64123f16999741162fe2
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:54:15 -0700

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "Region to create resources in"
+  type        = string
+  default     = "sfo3"
+}
+
+variable "droplet_size" {
+  description = "Droplet size slug"
+  type        = string
+  default     = "s-1vcpu-1gb"
+}
