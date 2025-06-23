97201970d5e5b1213a79fdb1712a788dac2a2322
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:23:11 -0700

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,10 @@
+# infra/versions.tf
 terraform {
   required_version = ">= 1.4"
-
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.55"   # lock to the newest 2.55.x patch
+      version = "~> 2.55"
     }
   }
 }
