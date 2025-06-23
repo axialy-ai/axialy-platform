8be8207fbd20851f5f77e81e65b273740d175b6c
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-22 20:19:29 -0700

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,11 +1,14 @@
 terraform {
-  required_version = ">= 1.4"
+  required_version = ">= 1.8.0"
 
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      # Stay on the newest 2.55.x patch (2.56.0 doesn’t exist yet)
-      version = "~> 2.55"
+      version = "~> 2.37"
+    }
+    namesilo = {
+      source  = "namesilo/namesilo"
+      version = "~> 1.4"
     }
   }
 }
