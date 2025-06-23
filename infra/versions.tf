da5d273570a8602411f9283d64b60c8cb47f4e47
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:12:19 -0700

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -4,18 +4,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.37"
-    }
-
-    # community provider published on the registry
-    namesilo = {
-      source  = "skitionek/namesilo"
-      version = "~> 1.4"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
+      version = "~> 2.55"   # lock to the newest 2.55.x patch
     }
   }
 }
