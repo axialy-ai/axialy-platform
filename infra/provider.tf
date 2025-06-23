308727ae679883406c526fde2dd5419d29665801
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-22 19:35:30 -0700

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,22 @@
+terraform {
+  required_version = ">= 1.4"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.55"
+    }
+    namesilo = {
+      source  = "namesilo/namesilo"
+      version = "~> 1.2"
+    }
+  }
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
 
+provider "namesilo" {
+  api_key = var.ns_key
+}
