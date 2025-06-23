835d4a4082dcc637d5e6951ceaac4b4f25cc7854
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-22 20:24:14 -0700

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,8 +1,8 @@
-# ── Provider configuration ─────────────────────────────────────────────────────
 provider "digitalocean" {
   token = var.do_token
 }
 
 provider "namesilo" {
   api_key = var.ns_key
+  domain  = var.ns_domain
 }
