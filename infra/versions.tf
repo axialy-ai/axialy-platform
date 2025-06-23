d0fa18cabd212a4a4aed25c60a1f203eb4b0bb5a
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:31:00 -0700

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,15 @@
 # infra/versions.tf
 terraform {
   required_version = ">= 1.4"
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.55"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
     }
   }
 }
