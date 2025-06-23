0a92d2927c76971515310490f9f8ecbb3308f731
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-23 10:54:36 -0700

    Create versions.tf

---
+++ infra/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.4"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.55"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+}
