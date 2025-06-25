60f7ac5b6f987e1d63d88ddca0f46dad889bf254
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-25 15:02:22 -0700

    Update template_static_site.tf

--- infra/template_static_site.tf
+++ infra/template_static_site.tf
@@ -1,11 +1,16 @@
 # infra/template_static_site.tf
-data "template_file" "static_site" {
-  template = file("${path.module}/cloudinit/static_site.tpl")
-  vars = {
-    admin_db_host     = digitalocean_database_cluster.mysql.host
-    admin_db_port     = digitalocean_database_cluster.mysql.port
-    admin_db_name     = digitalocean_database_db.admin.name
-    admin_db_user     = digitalocean_database_user.admin_app.name
-    admin_db_password = digitalocean_database_user.admin_app.password
-  }
+##############################
+
+# Render the cloud-init YAML once, injecting DB creds pulled from DO resources.
+locals {
+  static_site_user_data = templatefile(
+    "${path.module}/cloudinit/static_site.tpl",
+    {
+      admin_db_host     = digitalocean_database_cluster.mysql.host
+      admin_db_port     = digitalocean_database_cluster.mysql.port
+      admin_db_name     = digitalocean_database_db.admin.name
+      admin_db_user     = digitalocean_database_user.admin_app.name
+      admin_db_password = digitalocean_database_user.admin_app.password
+    }
+  )
 }
