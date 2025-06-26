083bd00b1affc673dd2f0f587dab68211b326f31
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-25 17:22:32 -0700

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,31 @@
-########################################
-# Public IPv4 addresses for each host  #
-########################################
+############################################
+# Public IPv4 addresses for every droplet #
+############################################
 output "droplet_ips" {
-  description = "Public IPv4 for every Axialy droplet"
+  description = "Public IPv4 for each Axialy droplet"
   value       = { for k, d in digitalocean_droplet.sites : k => d.ipv4_address }
 }
 
-########################################
-# Admin-app MySQL connection details   #
-########################################
-output "admin_db_host"     { value = digitalocean_database_cluster.mysql.host }
-output "admin_db_port"     { value = digitalocean_database_cluster.mysql.port }
-output "admin_db_user"     { value = digitalocean_database_user.admin_app.name }
-output "admin_db_password" { value = digitalocean_database_user.admin_app.password sensitive = true }
-output "admin_db_name"     { value = digitalocean_database_db.admin.name }
+############################################
+# Admin-app MySQL connection details       #
+############################################
+output "admin_db_host" {
+  value = digitalocean_database_cluster.mysql.host
+}
+
+output "admin_db_port" {
+  value = digitalocean_database_cluster.mysql.port
+}
+
+output "admin_db_user" {
+  value = digitalocean_database_user.admin_app.name
+}
+
+output "admin_db_password" {
+  value     = digitalocean_database_user.admin_app.password
+  sensitive = true
+}
+
+output "admin_db_name" {
+  value = digitalocean_database_db.admin.name
+}
