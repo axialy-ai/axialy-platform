5d56d4fc146651643499e5f25e1809e8b2441470
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-25 23:31:21 -0700

    Delete infra directory

--- infra/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "droplet_ips" {
-  value = {
-    admin = digitalocean_droplet.admin.ipv4_address
-    ui    = digitalocean_droplet.ui.ipv4_address
-    api   = digitalocean_droplet.api.ipv4_address
-    root  = digitalocean_droplet.root.ipv4_address
-  }
-}
