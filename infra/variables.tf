bd7d017c65de316d43cdb94f1af3cd6119f6b40c
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-29 20:15:00 -0700

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,9 @@
 variable "do_token" {
   description = "DigitalOcean API token"
   type        = string
-  sensitive   = true
 }
 
 variable "ssh_fingerprint" {
-  description = "Fingerprint of the deploy SSH key (shown in DO → Settings → Security)"
+  description = "Fingerprint of the SSH key that will access the droplet"
   type        = string
-}
-
-variable "region" {
-  type    = string
-  default = "sfo3"
-}
-
-variable "droplet_size" {
-  type    = string
-  default = "s-1vcpu-1gb"
 }
