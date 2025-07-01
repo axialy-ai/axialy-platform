9a623fdc41c843c2dd9156fd62ee4f79a1b8c25d
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-06-30 23:42:40 -0700

    Delete infra directory

--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "do_token" {
-  description = "DigitalOcean API token"
-  type        = string
-}
-
-variable "ssh_fingerprint" {
-  description = "Fingerprint of the SSH key used for the droplet"
-  type        = string
-}
