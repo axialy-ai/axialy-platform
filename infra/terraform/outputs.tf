0f91b1b1033f13c76a696103daf5f02e89b56970
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-02 16:23:52 -0700

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,10 +1,8 @@
 output "droplet_ip"  { value = digitalocean_droplet.admin.ipv4_address }
 output "db_host"     { value = digitalocean_database_cluster.axialy.host }
 output "db_port"     { value = digitalocean_database_cluster.axialy.port }
-
-output "db_username" { value = digitalocean_database_user.admin.name }
-
+output "db_username" { value = digitalocean_database_user.svc.name }
 output "db_password" {
-  value     = digitalocean_database_user.admin.password
+  value     = digitalocean_database_user.svc.password
   sensitive = true
 }
