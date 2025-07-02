9c8fcecefdb49a69f0bf959703ccf8f26ff4d71f
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-02 16:23:02 -0700

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,12 +1,7 @@
-variable "do_token"        { type = string }
-variable "ssh_pub_key"     { type = string }
-variable "ssh_key_name"    { type = string default = "axialy-admin" }
+variable "do_token"  { type = string }
+variable "pub_key"   { type = string }          # SSH key for droplet
+variable "region"    { type = string }
 
-variable "droplet_region"  { type = string default = "nyc1" }
-variable "droplet_size"    { type = string default = "s-1vcpu-1gb" }
-variable "droplet_image"   { type = string default = "docker-20-04" }
-
-variable "namesilo_api_key" { type = string }
-variable "domain_name"      { type = string }
-
-variable "mysql_version"    { type = string default = "8" }
+# droplet size & image – adjust if needed
+variable "droplet_size"  { default = "s-1vcpu-1gb" }
+variable "droplet_image" { default = "docker-20-04" }
