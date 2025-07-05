a6ba1138614f67dd9aca35227dd63410f73a8bac
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-05 15:23:38 -0700

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,46 +1,45 @@
 ###############################################################################
+#  Outputs – **one definition each**, clearly separated
+###############################################################################
+
+#############################
 #  Droplet
-###############################################################################
+#############################
 output "droplet_ip" {
-  value = digitalocean_droplet.admin.ipv4_address
+  description = "Public IPv4 of the admin droplet"
+  value       = digitalocean_droplet.admin.ipv4_address
 }
 
-###############################################################################
-#  Admin DB
-###############################################################################
+#############################
+#  Axialy_ADMIN credentials
+#############################
 output "admin_db_host" {
-  value = digitalocean_database_cluster.axialy.host
+  value       = digitalocean_database_cluster.axialy.host
 }
-
 output "admin_db_port" {
-  value = digitalocean_database_cluster.axialy.port
+  value       = digitalocean_database_cluster.axialy.port
 }
-
 output "admin_db_username" {
-  value = digitalocean_database_user.admin_user.name
+  value       = digitalocean_database_user.admin_user.name
 }
-
 output "admin_db_password" {
-  value     = digitalocean_database_user.admin_user.password
-  sensitive = true
+  value       = digitalocean_database_user.admin_user.password
+  sensitive   = true
 }
 
-###############################################################################
-#  UI DB
-###############################################################################
+#############################
+#  Axialy_UI credentials
+#############################
 output "ui_db_host" {
-  value = digitalocean_database_cluster.axialy.host
+  value       = digitalocean_database_cluster.axialy.host
 }
-
 output "ui_db_port" {
-  value = digitalocean_database_cluster.axialy.port
+  value       = digitalocean_database_cluster.axialy.port
 }
-
 output "ui_db_username" {
-  value = digitalocean_database_user.ui_user.name
+  value       = digitalocean_database_user.ui_user.name
 }
-
 output "ui_db_password" {
-  value     = digitalocean_database_user.ui_user.password
-  sensitive = true
+  value       = digitalocean_database_user.ui_user.password
+  sensitive   = true
 }
