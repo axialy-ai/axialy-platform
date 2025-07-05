4584c70e0b68c370566bb93637251c1fa4160337
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-05 15:51:40 -0700

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,4 @@
+# infra/terraform/main.tf
 ###############################################################################
 #  Axialy Admin-stack resources – NO outputs here, they live in outputs.tf
 ###############################################################################
@@ -5,7 +6,10 @@
 #############################
 #  SSH key for droplet
 #############################
+# When `ssh_pub_key` is provided, create a new key.
+# Otherwise we rely on an existing key supplied via `ssh_key_id`.
 resource "digitalocean_ssh_key" "axialy" {
+  count      = var.ssh_pub_key != "" ? 1 : 0
   name       = "axialy-key"
   public_key = var.ssh_pub_key
 }
@@ -14,15 +18,15 @@ resource "digitalocean_ssh_key" "axialy" {
 #  Managed MySQL cluster
 #############################
 resource "digitalocean_database_cluster" "axialy" {
-  name       = "axialy-cluster"
-  engine     = "mysql"
-  version    = "8"
-  region     = var.region
-  size       = "db-s-1vcpu-1gb"
-  node_count = 1
+  name        = "axialy-cluster"
+  engine      = "mysql"
+  version     = "8"
+  region      = var.region
+  size        = "db-s-1vcpu-1gb"
+  node_count  = 1
 }
 
-# Two logical databases in the same cluster
+# Logical databases
 resource "digitalocean_database_db" "admin" {
   cluster_id = digitalocean_database_cluster.axialy.id
   name       = "Axialy_ADMIN"
@@ -33,7 +37,7 @@ resource "digitalocean_database_db" "ui" {
   name       = "Axialy_UI"
 }
 
-# Separate users for each logical DB
+# Separate users
 resource "digitalocean_database_user" "admin_user" {
   cluster_id = digitalocean_database_cluster.axialy.id
   name       = "axialy_admin_user"
@@ -45,7 +49,7 @@ resource "digitalocean_database_user" "ui_user" {
 }
 
 #############################
-#  Droplet that hosts the Admin container
+#  Droplet hosting the Admin stack
 #############################
 resource "digitalocean_droplet" "admin" {
   name       = "axialy-admin-droplet"
@@ -54,7 +58,13 @@ resource "digitalocean_droplet" "admin" {
   image      = "ubuntu-22-04-x64"
   ipv6       = true
   monitoring = true
-  ssh_keys   = [digitalocean_ssh_key.axialy.id]
+
+  # Choose the SSH key:
+  #   • use existing key if `ssh_key_id` is set
+  #   • otherwise use the key we just created
+  ssh_keys = var.ssh_key_id != "" ?
+    [var.ssh_key_id] :
+    [digitalocean_ssh_key.axialy[0].id]
 
   # cloud-init installs Docker and pulls the GHCR image
   user_data = file("${path.module}/cloud-init.yml")
