39bd128fc15dac6a7cdcb5b9c89768f8dff0f2d9
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-05 15:14:41 -0700

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,21 +1,46 @@
+###############################################################################
+#  Droplet
+###############################################################################
 output "droplet_ip" {
-  description = "Public IPv4 of the admin droplet"
-  value       = digitalocean_droplet.admin.ipv4_address
+  value = digitalocean_droplet.admin.ipv4_address
 }
 
-output "db_host" {
-  value       = digitalocean_database_cluster.axialy.host
+###############################################################################
+#  Admin DB
+###############################################################################
+output "admin_db_host" {
+  value = digitalocean_database_cluster.axialy.host
 }
 
-output "db_port" {
-  value       = digitalocean_database_cluster.axialy.port
+output "admin_db_port" {
+  value = digitalocean_database_cluster.axialy.port
 }
 
-output "db_username" {
-  value       = digitalocean_database_user.admin_user.name
+output "admin_db_username" {
+  value = digitalocean_database_user.admin_user.name
 }
 
-output "db_password" {
-  value       = digitalocean_database_user.admin_user.password
-  sensitive   = true
+output "admin_db_password" {
+  value     = digitalocean_database_user.admin_user.password
+  sensitive = true
+}
+
+###############################################################################
+#  UI DB
+###############################################################################
+output "ui_db_host" {
+  value = digitalocean_database_cluster.axialy.host
+}
+
+output "ui_db_port" {
+  value = digitalocean_database_cluster.axialy.port
+}
+
+output "ui_db_username" {
+  value = digitalocean_database_user.ui_user.name
+}
+
+output "ui_db_password" {
+  value     = digitalocean_database_user.ui_user.password
+  sensitive = true
 }
