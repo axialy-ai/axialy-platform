2a0ad955568bf95de40cf0a90bdad1942d5c3ed5
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-05 14:56:59 -0700

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,3 +1,7 @@
+###############################################################################
+#  Axialy Platform – Terraform VARIABLES
+###############################################################################
+
 variable "do_token" {
   description = "DigitalOcean API token"
   type        = string
@@ -5,7 +9,13 @@ variable "do_token" {
 }
 
 variable "ssh_pub_key" {
-  description = "Public-key string used to log in to the droplet"
+  description = "Public-key string (only used when you really need to create a key)"
+  type        = string
+  default     = ""
+}
+
+variable "ssh_key_id" {
+  description = "ID of an existing DigitalOcean SSH key to inject into the Droplet"
   type        = string
 }
 
@@ -13,4 +23,11 @@ variable "region" {
   description = "DO region"
   type        = string
   default     = "nyc3"
+}
+
+variable "ssh_pub_key_unused" {
+  description = "Kept only for backward compatibility – do not use."
+  type        = string
+  default     = ""
+  sensitive   = true
 }
