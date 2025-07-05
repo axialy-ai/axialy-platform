638474325373c5c64c0e85f780d1770a79dda9e8
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-05 14:58:11 -0700

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,18 +1,9 @@
 ###############################################################################
 #  Axialy Platform – Terraform MAIN
 #  ---------------------------------------------------------------------------
-#  • Only *resources* live here.
-#  • Provider configurations and outputs have been moved to separate files
-#    to avoid any duplication errors.
+#  This file intentionally assumes the SSH key already exists in DigitalOcean
+#  (we detect it in the GitHub workflow and pass its ID via var.ssh_key_id).
 ###############################################################################
-
-#################################
-#  SSH key (imported or created)
-#################################
-resource "digitalocean_ssh_key" "axialy" {
-  name       = "axialy-key"
-  public_key = var.ssh_pub_key
-}
 
 #################################
 #  Managed MySQL cluster
@@ -59,11 +50,24 @@ resource "digitalocean_droplet" "admin" {
   ipv6       = true
   monitoring = true
 
-  ssh_keys   = [digitalocean_ssh_key.axialy.id]
+  # the key has been created previously – just reference its ID
+  ssh_keys   = [var.ssh_key_id]
 
   user_data = <<EOF
 #cloud-config
 package_update: true
 package_upgrade: true
 EOF
+}
+
+###############################################################################
+#  Outputs consumed by the GitHub workflow
+###############################################################################
+output "droplet_ip"  { value = digitalocean_droplet.admin.ipv4_address }
+output "db_host"     { value = digitalocean_database_cluster.axialy.host }
+output "db_port"     { value = digitalocean_database_cluster.axialy.port }
+output "db_username" { value = digitalocean_database_user.admin_user.name }
+output "db_password" {
+  value     = digitalocean_database_user.admin_user.password
+  sensitive = true
 }
